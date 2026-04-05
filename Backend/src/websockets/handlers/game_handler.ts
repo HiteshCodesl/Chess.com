2dfe32d0c3c7bda6ef2d3369f8db7a28e87a7377
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-05 17:22:19 +0530

    added the matchmaking

--- Backend/src/websockets/handlers/game_handler.ts
+++ Backend/src/websockets/handlers/game_handler.ts
@@ -1,10 +1,30 @@
 import type { User } from "./connection_handler.js";
+import { prisma } from "../../lib/prisma.js";
+import { roomManager } from "./room_handler.js";
 
-export function createGame(user1: User, user2: User){
-    const blackPlayer = user1;
-    const whitePlayer = user2;
-    
-    blackPlayer.socket.send({
-        "message": "User Found"
+export async function createGame(waitingUser: User, currentUser: User) {
+   
+    const createGame = await prisma.game.create({
+        data: {
+            blackId: currentUser.id,
+            whiteId: waitingUser.id,
+            status: "WAITING"
+        }
     })
+
+    const gameId = createGame.id;
+
+    currentUser.socket.send(JSON.stringify({
+        "event": "matchFound",
+        "gameId": gameId,
+        "color": "black"
+    }));
+
+    waitingUser.socket.send(JSON.stringify({
+        "event": "matchFound",
+        "gameId": gameId,
+        "color": "white"
+    }));
+
+    roomManager(currentUser, waitingUser, gameId);
 }
