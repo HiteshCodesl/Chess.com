94d603cee0cde8efbc6676824780faaa0b204bae
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-08 12:29:44 +0530

    added the route handlers

---
+++ Backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import { userRouter } from "./modules/User/user.js";
+import { gameRouter } from "./modules/Game/Game.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/game', gameRouter);
+
+export default app;
