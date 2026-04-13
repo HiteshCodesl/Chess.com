60a5aa32b04559d6322d5e2d5e71f68f7105da1e
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-13 18:42:58 +0530

    made the mvp level chess app for singlePlayer

--- Frontend/src/components/dashboard/ChessBoard.tsx
+++ Frontend/src/components/dashboard/ChessBoard.tsx
@@ -2,21 +2,23 @@ import { IoFlash } from "react-icons/io5";
 import { LiaRobotSolid } from "react-icons/lia";
 import { IoPeople } from "react-icons/io5";
 import { FaChessKnight } from "react-icons/fa6";
-import { useSocketContext } from "../../context/useSocketContext";
+import { useContext } from "react";
 import getUserDetails from "../../context/getUserDetailsContext";
-
+import Board from "./Board";
+import { SocketContext } from "../../context/useSocketContext";
 
 function ChessBoard() {
-  const socket = useSocketContext();
+  const socket = useContext(SocketContext);
   const {user}  = getUserDetails();
- console.log(user);
+ console.log("user", user);
+
   return (
     <div className="min-h-screen flex justify-center bg-[#302f2c]">
 
-      <div className="w-full max-w-6xl grid grid-cols-3 gap-6 p-6">
+      <div className="w-full max-w-6xl grid grid-cols-3 gap-6 p-6 justify-between">
 
         <div className="col-span-2 bg-[#302f2c]">
-
+            <Board />
         </div>
 
         <div className="col-span-1 bg-[#262522] flex flex-col gap-4 p-4">
--- Frontend/src/components/dashboard/Square.tsx
+++ Frontend/src/components/dashboard/Square.tsx
@@ -1,17 +1,19 @@
+import { PieceMap, type PieceKey } from "../../utils/Peice"; // adjust path
 
-function Square({piece, square, onClick}:any) {
-    const isBlack = (square.charCodeAt(0) + Number(square[1])) % 2 === 0;
+function Square({ piece, square, onClick }: any) {
+  const isBlack =
+    (square.charCodeAt(0) + Number(square[1])) % 2 === 0;
 
   return (
     <div
       onClick={() => onClick(square)}
-      className={`w-18 h-14 flex items-center justify-center ${
+      className={`w-16 h-16 flex items-center justify-center text-5xl ${
         isBlack ? "bg-green-700" : "bg-white"
       }`}
     >
-      {piece && piece.type}
+      {piece ? PieceMap[`${piece.type}${piece.color}` as PieceKey] : null}
     </div>
-  )
+  );
 }
 
-export default Square
+export default Square;
