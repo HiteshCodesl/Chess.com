534e6f849c8331a48a2c935b9d501d5641bb18b1
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-16 16:57:56 +0530

    added the transction to move and game fen storing in db and also added isCheck condition

--- Backend/src/websockets/handlers/move_handler.ts
+++ Backend/src/websockets/handlers/move_handler.ts
@@ -3,104 +3,127 @@ import { getGame } from "./room_handler.js";
 import type WebSocket from "ws";
 
 export async function moveHandler(from: string, to: string, gameId: string, socket: WebSocket) {
-    try{
-    const game = getGame(gameId);
-    let winner: string;
+    try {
+        const game = getGame(gameId);
+        let winner: string;
 
-    if (!game) {
-        console.log("game not Found");
-        return;
-    }
-
-    const chess = game.chess;
-
-    const blackPlayerSocket = game.players[0]!.socket;
-    const whitePlayerSocket = game.players[1]!.socket;
-
-    if (chess.turn() === 'w' && socket !== whitePlayerSocket) {
-        console.log("Invalid Move, White players turn");
-        return;
-    }
-
-    if (chess.turn() === 'b' && socket !== blackPlayerSocket) {
-        console.log("Invalid Move, Black players turn");
-        return;
-    }
-
-    const move = chess.move({
-        from,
-        to
-    })
-
-    console.log("Move", move);
-
-    if (!move) {
-        console.log("Move not made");
-        return;
-    }
-
-    game.players.forEach((player) => {
-        player.socket.send(JSON.stringify({
-            "event": "MOVE",
-            from,
-            to,
-            fen: chess.fen()
-        }))
-    })
-
-    await prisma.move.create({
-        data: {
-            from,
-            to,
-            time: new Date(),
-            gameId: gameId
+        if (!game) {
+            console.log("game not Found");
+            return;
         }
-    })
 
+        const chess = game.chess;
 
-    if (chess.isGameOver()) {
-        const turn = chess.turn();
-        winner = turn === 'w' ? 'black' : 'white';
+        const blackPlayerSocket = game.players.blackPlayer.socket;
+        const whitePlayerSocket = game.players.whitePlayer.socket;
 
-        game.players.forEach((player) => {
-            player.socket.send(JSON.stringify({
+        const blackPlayerId = game.players.blackPlayer.id;
+        const whitePlayerId = game.players.whitePlayer.id;
+
+        if (chess.turn() === 'w' && socket !== whitePlayerSocket) {
+            console.log("Invalid Move, White players turn");
+            return;
+        }
+
+        if (chess.turn() === 'b' && socket !== blackPlayerSocket) {
+            console.log("Invalid Move, Black players turn");
+            return;
+        }
+
+        
+
+        const move = chess.move({
+            from,
+            to
+        })
+
+        console.log("Move", move);
+
+        if (!move) {
+            console.log("Move not made");
+            return;
+        }
+
+        const blackandWhitePlayerSockets: WebSocket | null = game.players.whitePlayer.socket && game.players.blackPlayer.socket;
+
+        if (chess.isGameOver()) {
+            const turn = chess.turn();
+            winner = turn === 'w' ? 'black' : 'white';
+
+            blackandWhitePlayerSockets?.send(JSON.stringify({
                 "event": "GAME_ENDED",
                 result: chess.isCheckmate() ? "CheckMate" : "Draw",
                 winner
             }))
-        })
 
-        await prisma.game.update({
-            where: {
-                id: gameId
-            },
-            data: {
-                winnerId: '123', // to be calculated using winner,
-                status: 'FINISHED'
-            }
-        })
-    }
 
-    if (chess.isDraw()) {
-        game.players.forEach((player) => {
-            player.socket.send(JSON.stringify({
+            await prisma.game.update({
+                where: {
+                    id: gameId
+                },
+                data: {
+                    winnerId: chess.turn() === 'w' ? blackPlayerId : whitePlayerId,
+                    status: 'FINISHED'
+                }
+            })
+        }
+
+        if (chess.isDraw()) {
+
+            blackandWhitePlayerSockets?.send(JSON.stringify({
                 result: 'Draw'
             }))
-        })
 
-        await prisma.game.update({
-            where: {
-                id: gameId
-            },
+
+            await prisma.game.update({
+                where: {
+                    id: gameId
+                },
+                data: {
+                    status: "DRAW"
+                }
+            })
+        }
+
+        if(chess.isCheck()){
+            if(chess.isCheck())
+            blackandWhitePlayerSockets?.send(JSON.stringify({
+                "event": "CHECK"
+            }))
+        }
+        
+
+        blackandWhitePlayerSockets?.send(JSON.stringify({
+            "event": "MOVE",
+            from,
+            to,
+            fen: chess.fen()
+        }));
+
+    await prisma.$transaction([
+
+        prisma.move.create({
             data: {
-                status: "DRAW"
+                from,
+                to,
+                time: new Date(),
+                gameId: gameId
             }
-        })
-    }
+        }),
 
-  }catch(err){
-    console.log("Invalid Move");
-    return;
-  }
+        prisma.game.update({
+           where: {
+             id: gameId
+           },
+           data: {
+            fen: chess.fen()
+           }
+        })
+    ])
+
+    } catch (err) {
+        console.log("Invalid Move");
+        return;
+    }
 }
 
