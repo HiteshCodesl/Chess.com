609c07d7be2dd79afd70723fd9271fe6ae0395c1
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-17 16:45:16 +0530

    added the game Interface and also added if game not found in memoery then server is crashed rebuild game called

--- Backend/src/websockets/handlers/connection_handler.ts
+++ Backend/src/websockets/handlers/connection_handler.ts
@@ -2,12 +2,27 @@ import WebSocket from "ws";
 import { createGame } from "./game_handler.js";
 import { prisma } from "../../lib/prisma.js";
 import { JoinExistingGame, type Role } from "./joinExistingGame_handler.js";
+import { getGame } from "./room_handler.js";
+import { rebuildGame } from "./rebuildGame_handler.js";
+import type { GameStatus } from "@prisma/client";
 
 export interface User {
     id: string;
-    name: string;
+    name: string | 'anonymous';
     socket: WebSocket | null;
 };
+
+export interface Game {
+    id: string;
+    whiteId: string;
+    blackId: string;
+    fen: string;
+    createdAt: Date;
+    status: GameStatus;
+    winnerId: string | null;
+    winningReason: string | null
+}
+
 
 export let globalJoiningQueue: User[] = [];
 
@@ -25,55 +40,64 @@ export async function joinRoomInit(socket: WebSocket, payload: any) {
         where: {
             status: 'ACTIVE',
             OR: [
-                {blackId: id},
-                {whiteId: id}
+                { blackId: id },
+                { whiteId: id }
             ]
         }
     })
 
     let role: Role;
 
-    if(findRunningGame){
+    if (findRunningGame) {
 
-    if(findRunningGame.blackId === id){
-        role = 'blackPlayer';
-    }else if(findRunningGame.whiteId === id){
-        role = 'whitePlayer';
-    }else{
-        return;
-    }
+        const gameId = findRunningGame.id;
+        const fen = findRunningGame.fen ?? '';
+        const game = getGame(gameId);
 
-    const gameId = findRunningGame.id;
-    const fen  = findRunningGame.fen ?? '';
-
-    JoinExistingGame(gameId, role, currentUser, fen);
-
-   }else{
-
-    if (globalJoiningQueue.length === 0) {
-        globalJoiningQueue.push({
-            id,
-            name,
-            socket
-        })
-        console.log(globalJoiningQueue);
-    }
-    else if (globalJoiningQueue.length > 0) {
-        //Removes first element and deletes it.
-        const waitingUser = globalJoiningQueue.shift();
-        
-        if (!waitingUser) {
-            console.log("User Not Found")
-            return;
+         if (findRunningGame.blackId === id) {
+                role = 'blackPlayer';
+            } else if (findRunningGame.whiteId === id) {
+                role = 'whitePlayer';
+            } else {
+                return;
         }
 
-        if(waitingUser.id === currentUser.id){
-            console.log("Same User");  
-            globalJoiningQueue.push(waitingUser);
-            return;
+        if (!game) {
+            //server is crashed
+            rebuildGame(gameId, role, findRunningGame, currentUser);
+            
+        } else {
+            //player is disconnected
+
+            JoinExistingGame(gameId, role, currentUser, fen);
         }
-    
-        createGame(waitingUser, currentUser);
-     }
-   }
+
+    } else {
+
+        if (globalJoiningQueue.length === 0) {
+            globalJoiningQueue.push({
+                id,
+                name,
+                socket
+            })
+            console.log(globalJoiningQueue);
+        }
+        else if (globalJoiningQueue.length > 0) {
+            //Removes first element and deletes it.
+            const waitingUser = globalJoiningQueue.shift();
+
+            if (!waitingUser) {
+                console.log("User Not Found")
+                return;
+            }
+
+            if (waitingUser.id === currentUser.id) {
+                console.log("Same User");
+                globalJoiningQueue.push(waitingUser);
+                return;
+            }
+
+            createGame(waitingUser, currentUser);
+        }
+    }
 }
