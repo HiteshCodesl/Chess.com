17848542e809bb91875887124266a0e8bb7b327c
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-23 20:21:16 +0530

    added the redis

--- Backend/src/config/redis.ts
+++ Backend/src/config/redis.ts
@@ -2,9 +2,13 @@ import {Redis} from "ioredis"
 
 export const redis = new Redis({
     port: 6379,
-    host: "localhost"
+    host: "127.0.0.1"
 })
 
 redis.on("connect", () => {
   console.log("Redis connected");
+});
+
+redis.on("error", (err) => {
+  console.log("Redis error:", err);
 });
