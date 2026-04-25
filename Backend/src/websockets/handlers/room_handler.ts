ee1d3964118002627ad2be77bbaac8cf39d9993c
Hitesh Navare <navarehitesh65@gmail.com>
2026-04-25 15:44:10 +0530

    added the clock to know the time for each player

--- Backend/src/websockets/handlers/room_handler.ts
+++ Backend/src/websockets/handlers/room_handler.ts
@@ -4,24 +4,31 @@ import type { User } from './connection_handler.js';
 type GameRoom = {
     gameId: string;
     players: {
-        whitePlayer : User,
-        blackPlayer : User
+        whitePlayer: User,
+        blackPlayer: User
     }
     chess: Chess;
+    clock: {
+        whiteTime: number;
+        blackTime: number;
+        lastMoveTime: number;
+        turn: "w" | "b";
+    } | null;
 };
 
 export const games = new Map<string, GameRoom>();
 
-export async function roomHandler(blackPlayer: User, whitePlayer: User, gameId: string) {
+export async function roomHandler(blackPlayer: User, whitePlayer: User, gameId: string, gameTime: number) {
     const chess = new Chess();
-    
+
     games.set(gameId, {
         gameId,
         chess,
-        players : {
-        whitePlayer,
-        blackPlayer, 
-        }
+        players: {
+            whitePlayer,
+            blackPlayer,
+        },
+        clock: null
     })
 }
 
